d27c827118a32ff07d7725a4633ecddee156236d
tofuliang <tofuliang@gmail.com>
2017-06-20 00:34:10 +0800

    add zimuku shortcut

--- Subhd-shortcut-for-RARBG.user.js
+++ Subhd-shortcut-for-RARBG.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Subhd for RARBG
+// @name         Subhd,Zimuku for RARBG
 // @namespace    http://rarbg.to/
-// @version      0.2
-// @description  Adds Subhd shortcut to RARBG.to
+// @version      0.3
+// @description  Adds Subhd,Zimuku shortcut to RARBG.to
 // @author       tofuliang
 // @match        https://rarbg.to/*
 // @match        http://rarbg.to/*
@@ -11,15 +11,21 @@
 // ==/UserScript==
 
 $(document).on('ready AutoPagerize_DOMNodeInserted', function(e) {
-	$('a[href^="/torrents.php?imdb="][subhded!=subhded]').each(function() {
-		var title = document.location.href.match(/https?:\/\/rarbg\.to\/torrent\/\w+/)?$('body > table > tbody > tr > td > div > div > table > tbody > tr > td > b > h1').text():$(this).parent('td').find('a').text(),
-			parts = title.replace(/\s+/g,'.').split('.');
+	$('a[href^="/torrents.php?imdb="][subed!=subed]').each(function() {
+		var title = document.location.href.match(/https?:\/\/rarbg\.\w+\/torrent\/\w+/)?$('body > table > tbody > tr > td > div > div > table > tbody > tr > td > b > h1').text():$(this).parent('td').find('a').text(),
+			parts =  title.replace(/\s+/g,'.').split('.');
+		var partsBak = parts.slice(0);
 		while(parts.length > 0 && (part = parts.pop().match(/^\d{4}$/)) === null){}
+		if(parts === null || parts.length < 1){
+			parts = partsBak.slice(0);
+			while(parts.length > 0 && (part = parts.pop().match(/^s\d{1,2}e?\d{0,2}$/i)) === null){}
+		}
 		parts.push(part);
 		var keyword = parts.join('.'),
-			url='http://subhd.com/search/'+keyword;
-		$(this).after('<a style="margin-left:5px;" href='+url+' target="_blank" onmouseover="return overlib(\'<span>去subhd找字幕</span>\')" onmouseout="return nd();"><img src="http://subhd.com/images/favicon-32x32.png" style="height:18px;"></img></a>');
-		$(this).attr('subhded','subhded');
+			subhdUrl='http://subhd.com/search/'+keyword,
+			zimuzuUrl='http://www.zimuku.net/search?q='+keyword;
+		$(this).after('<a style="margin-left:5px;" href='+subhdUrl+' target="_blank" onmouseover="return overlib(\'<span>去subhd找字幕</span>\')" onmouseout="return nd();"><img src="http://subhd.com/images/favicon-32x32.png" style="height:18px;"></img></a><a style="margin-left:5px;" href='+zimuzuUrl+' target="_blank" onmouseover="return overlib(\'<span>去zimuzu找字幕</span>\')" onmouseout="return nd();"><img src="http://www.zimuku.net/images/v2/search.png" style="height:18px;"></img></a>');
+		$(this).attr('subed','subed');
 	});
 });
 $(document).bind("ready", function() {});
